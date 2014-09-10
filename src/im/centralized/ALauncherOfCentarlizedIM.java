8ab7484232c2551ec358f36eff1651f7399c31bf
pdewan <dewan@cs.unc.edu>
2014-09-10 11:12:07 -0400

    Added launchers but centralized does not seem to work

---
+++ src/im/centralized/ALauncherOfCentarlizedIM.java
@@ -0,0 +1,67 @@
+package im.centralized;
+
+import im.ALauncherOfIM;
+import im.LauncherOfIM;
+import util.tags.DistributedTags;
+import bus.uigen.pipe.AConsoleModelBasedLauncher;
+import bus.uigen.pipe.MainClassLaunchingUtility;
+
+public class ALauncherOfCentarlizedIM extends ALauncherOfIM implements LauncherOfIM {
+	
+	public Class[] mainClasses() {
+		return new Class[] { 
+				sessionManagerClass(), 
+				modelClass(),
+				aliceClass(), 
+				bobClass(),		
+				cathyClass() 
+			};
+	}
+	public String[] processNames() {
+		return new String[] {
+				sessionManagerName(), 		
+				aliceName(), 
+				bobName(),		
+				cathyName() 
+		};
+	}
+	public Class modelClass() {
+		return MasterIMModelLauncher.class;
+		// return OTSessionManagerServerStarter.class;
+
+	}
+	
+
+	public Class aliceClass() {
+		return AliceSlaveIM.class;
+		// return AliceOTIM.class;
+	}
+	
+
+	public Class bobClass() {
+		return BobSlaveIM.class;
+		// return BobOTIM.class;
+	}
+	
+
+	public Class cathyClass() {
+		return CathySlaveIM.class;
+		// return CathyOTIM.class;
+	}
+	
+	
+//	public  void launch() {	
+////		Class[] classes = {
+////				SessionManagerServerStarter.class,
+////				AliceIM.class,
+////				BobIM.class,
+////				CathyIM.class				
+////		};
+//		MainClassLaunchingUtility.createInteractiveLauncher(composeMainClasses());
+//	}
+	
+	public static void main(String args[]) {
+		(new ALauncherOfCentarlizedIM()).launchWithConsoles();
+	}
+
+}
