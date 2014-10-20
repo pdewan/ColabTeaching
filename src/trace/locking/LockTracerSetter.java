808c0d21b21f422758d51580288c0e6c2dadbac7
pdewan <dewan@cs.unc.edu>
2014-10-20 19:46:52 -0400

    More locking tracing

--- src/trace/locking/LockTracerSetter.java
+++ src/trace/locking/LockTracerSetter.java
@@ -14,12 +14,19 @@ public class LockTracerSetter extends EchoTracerSetter{
 	}
 	
 	public static void setLockPrintStatus() {
-		Tracer.setKeywordPrintStatus(InputRightRequested.class, true);	
 		Tracer.setKeywordPrintStatus(MasterLockGranted.class, true);
+		Tracer.setKeywordPrintStatus(MasterLockGrantRequestDenied.class, true);
+		Tracer.setKeywordPrintStatus(MasterLockGrantRequestReceived.class, true);
+		Tracer.setKeywordPrintStatus(MasterLockGrantStatusSent.class, true);
 		Tracer.setKeywordPrintStatus(MasterLockReleased.class, true);	
 		Tracer.setKeywordPrintStatus(MasterLockReleaseRequestReceived.class, true);	
+		Tracer.setKeywordPrintStatus(MasterLockReleaseStatusSent.class, true);
+
 		Tracer.setKeywordPrintStatus(SlaveLockGrantMadeReceived.class, true);
 		Tracer.setKeywordPrintStatus(SlaveLockGrantReceived.class, true);
+		Tracer.setKeywordPrintStatus(SlaveLockGrantRequestMade.class, true);
+		Tracer.setKeywordPrintStatus(SlaveLockReleaseReceived.class, true);
+		Tracer.setKeywordPrintStatus(SlaveLockReleaseRequestMade.class, true);
 		Tracer.setKeywordPrintStatus(SlaveLockReleaseRequestSent.class, true);
 		Tracer.setKeywordPrintStatus(SlaveLockGrantRequestSent.class, true);
 		Tracer.setKeywordPrintStatus(SlaveLockReleaseRequestMade.class, true);
