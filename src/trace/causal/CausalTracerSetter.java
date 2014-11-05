6a4b4f5df7b42fcceb1815f1ca89dbd0b1db9913
pdewan <dewan@cs.unc.edu>
2014-11-05 09:18:33 -0500

    More tracing

--- src/trace/causal/CausalTracerSetter.java
+++ src/trace/causal/CausalTracerSetter.java
@@ -14,10 +14,14 @@ public class CausalTracerSetter extends EchoTracerSetter{
 	}
 	
 	public static void setLockPrintStatus() {
+		Tracer.setKeywordPrintStatus(ConcurrentVectorTimeStampedMessageDetected.class, true);		
 		Tracer.setKeywordPrintStatus(LocalCountIncrementedInSiteVectorTimeStamp.class, true);
+		Tracer.setKeywordPrintStatus(RemoteCountIncrementedInSiteVectorTimeStamp.class, true);
 		Tracer.setKeywordPrintStatus(UserAddedToVectorTimeStamp.class, true);
+		Tracer.setKeywordPrintStatus(VectorTimeStampCopiedAndNewBufferCreatedInResponseToConcurrentMessage.class, true);
 		Tracer.setKeywordPrintStatus(VectorTimeStampCreated.class, true);
 		Tracer.setKeywordPrintStatus(VectorTimeStampedMessageBuffered.class, true);
+		Tracer.setKeywordPrintStatus(VectorTimeStampedMessageDelivered.class, true);
 		Tracer.setKeywordPrintStatus(VectorTimeStampedMessageReceived.class, true);	
 		Tracer.setKeywordPrintStatus(VectorTimeStampedMessageRemovedFromBuffer.class, true);	
 		Tracer.setKeywordPrintStatus(VectorTimeStampedMessageSent.class, true);		
